ac36d020e03cf57994936225584ec6ac58d81f90
Sourav Rakshit <38202655+souravrax@users.noreply.github.com>
2021-10-30 00:05:55 +0530

    Changed Some Files

--- Hello.java
+++ Hello.java
@@ -1,16 +1,7 @@
 import java.util.*;
-import java.io.IOException;
 
 class HelloWorld{
-
     public static void main(String ...arStrings) {
-        
-        System.out.println("Enter your name : ");
-        String name = System.console().readLine();
-        System.out.println("Your name is : " + name);
-        System.out.print("Enter your age : ");
-        int age = System.in.read();
-        System.out.println("Your age is : " + age);
-
+        System.out.println("Hello World");
     }
 }
